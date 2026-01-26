327ae5f60041920cc102b771f450cba4528c8cda
javierjsv <javierjsv@hotmail.com>
2026-01-26 16:25:00 -0500

    coctato

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonButtons, 
+  IonMenuButton,
+  IonCard,
+  IonButton,
+  IonIcon
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heart, logoInstagram, logoFacebook, logoWhatsapp, musicalNotes } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, ExploreContainerComponent],
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonButtons, 
+    IonMenuButton,
+    IonCard,
+    IonButton,
+    IonIcon
+  ],
 })
 export class Tab3Page {
-  constructor() {}
+  constructor() {
+    addIcons({ heart, logoInstagram, logoFacebook, logoWhatsapp, musicalNotes });
+  }
+
+  openSocialMedia(platform: string) {
+    // URLs de ejemplo - reemplazar con las URLs reales de las redes sociales
+    const urls: { [key: string]: string } = {
+      instagram: 'https://www.instagram.com/tu_panalera',
+      facebook: 'https://www.facebook.com/mgrantesoro/?locale=es_LA',
+      whatsapp: 'https://wa.me/3185704290',
+      tiktok: 'https://www.tiktok.com/@tu_panalera'
+    };
+
+    const url = urls[platform];
+    if (url) {
+      window.open(url, '_blank');
+    }
+  }
 }
